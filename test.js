f8774afc28fc8176b2678eae39ac7e1e4482b78b
AlekseyVoronkov <144922364+AlekseyVoronkov@users.noreply.github.com>
2025-10-04 09:54:21 +0700

    first commit, button spawning

---
+++ test.js
@@ -0,0 +1,6 @@
+// var colors = ["red", "blue", "black", "yellow"];
+let addButton = document.createElement('button');
+addButton.className = "addButton";
+addButton.textContent = "+";
+document.body.append(addButton);
+window.scrollBy(0, 20);
