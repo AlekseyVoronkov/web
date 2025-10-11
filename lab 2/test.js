5ad8b0962ab7b59d4185eaaeb38e5a8314154f90
AlekseyVoronkov <lemopnopad@gmail.com>
2025-10-11 13:03:10 +0700

    Added logic for task card and No tasks text spawning

--- lab 2/test.js
+++ lab 2/test.js
@@ -1,5 +1,76 @@
-// let addButton = document.createElement('button');
-// addButton.className = "addButton";
-// addButton.textContent = "+";
-// document.body.append(addButton);
-// window.scrollBy(0, 20);
+document.addEventListener('DOMContentLoaded', function() {
+    let addButton = document.querySelector('.addButton');
+    let titleInput = document.querySelectorAll('.textBox')[0];
+    let descriptionInput = document.querySelectorAll('.textBox')[1];
+    let tasksContainer = document.querySelector('.tasks');
+
+    updateNoTasksMessage();
+
+    function createTask(title, description) {
+        let taskDiv = document.createElement('div');
+        taskDiv.className = 'task';
+
+        let taskTextDiv = document.createElement('div');
+        taskTextDiv.className = 'task-text';
+        
+        let taskTitle = document.createElement('h1');
+        taskTitle.className = 'task-title';
+        taskTitle.textContent = title;
+        
+        let taskDesc = document.createElement('p');
+        taskDesc.className = 'task-desc';
+        taskDesc.textContent = description;
+        
+        let closeButton = document.createElement('button');
+        closeButton.className = 'closeButton';
+        closeButton.textContent = 'x';
+
+        taskTextDiv.appendChild(taskTitle);
+        taskTextDiv.appendChild(taskDesc);
+        taskDiv.appendChild(taskTextDiv);
+        taskDiv.appendChild(closeButton);
+        
+        tasksContainer.appendChild(taskDiv);
+        
+        closeButton.addEventListener('click', function() {
+            taskDiv.remove();
+            updateNoTasksMessage();
+        });
+        updateNoTasksMessage();
+    }
+
+    function addTask() {
+        let title = titleInput.value;
+        let description = descriptionInput.value;
+        
+        if(title.trim() === '' || description.trim() === '') {
+            // should make a modal window for that later ig
+            alert('Fill a title and description fields please!');
+            return;
+        }
+
+        createTask(title, description);
+        titleInput.value = '';
+        descriptionInput.value = '';
+    }
+
+    addButton.addEventListener('click', addTask);
+
+    function updateNoTasksMessage() {
+        let existingMessage = document.querySelector('.no-tasks-text');
+        let taskCounter = document.querySelectorAll('.task').length;
+        let noTasksContainer = document.querySelector('.no-tasks'); 
+
+        if (taskCounter === 0 && !existingMessage) {
+            let noTasksMessage = document.createElement('div');
+            noTasksMessage.className = 'no-tasks-text';
+            noTasksMessage.textContent = 'No tasks';
+
+            noTasksContainer.appendChild(noTasksMessage);
+            tasksContainer.appendChild(noTasksContainer);
+        }
+        else if (taskCounter != 0 && existingMessage) {
+            existingMessage.remove();
+        }
+    }
+})
