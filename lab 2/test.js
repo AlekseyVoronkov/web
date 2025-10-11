22f0dc0dcea5f1d4ed7ac568b0e187ea68d4eb3b
AlekseyVoronkov <lemopnopad@gmail.com>
2025-10-11 11:04:27 +0700

    atempt at making card template

---
+++ lab 2/test.js
@@ -0,0 +1,5 @@
+// let addButton = document.createElement('button');
+// addButton.className = "addButton";
+// addButton.textContent = "+";
+// document.body.append(addButton);
+// window.scrollBy(0, 20);
